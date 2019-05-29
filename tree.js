ed0ca1b67ef7dbf66a9a239edd2e870c3aa05425
WQM <244401893@qq.com>
2019-05-30 01:45:44 +0800

    rename function and variable

--- tree.js
+++ tree.js
@@ -66,50 +66,50 @@ const selectedNode = '2';
  * @param {Object} treeData 当前树节点数组
  * @param {String} selectedNode 当前选中的节点ID 
  */
-const traversingNode = (treeData, selectedNode) => {
-    let traversingStatus = false; 
+const traverseNode = (treeData, selectedNode) => {
+    let nodeStatus = false; 
     for (let i = 0; i < treeData.length; i++) {
         if (treeData[i].id === selectedNode) {
             // 当前对象为选中对象,更改选中状态，并立即通知父节点，不再查找其他节点，此同级节点循环结束
-            nodeSelected(treeData[i]);
+            setSelectedNodeStatus(treeData[i]);
             return true;
         } else if (treeData[i].children && treeData[i].children.length !== 0) {
             // 判断当前节点下是否有子节点
-            traversingStatus = traversingNode(treeData[i].children, selectedNode);
+            nodeStatus = traverseNode(treeData[i].children, selectedNode);
 
             // 根据查找状态，更改父节点选中状态
-            if (traversingStatus) {
-                nodeSelected(treeData[i]);
+            if (nodeStatus) {
+                setSelectedNodeStatus(treeData[i]);
             } else {
-                nodeUnSelected(treeData[i]);
+                setUnselectedNodeStatus(treeData[i]);
             }
         }else {
             // 当前未匹配到选中节点，并且也不存在子节点时，设置本身状态，并通知父节点，但需要查找其他节点是否满足条件
-            nodeUnSelected(treeData[i]);
-            traversingStatus = false;
+            setUnselectedNodeStatus(treeData[i]);
+            nodeStatus = false;
         }
     }
-    return traversingStatus
+    return nodeStatus
 }
 
-const nodeSelected = (node) => {
+const setSelectedNodeStatus = (node) => {
     return Object.assign(node, {
         selected: true,
         expand: true
     })
 }
 
-const nodeUnSelected = (node) => {
+const setUnselectedNodeStatus = (node) => {
     return Object.assign(node, {
         selected: false,
         expand: false
     })
 }
 
-traversingNode(data, selectedNode);
+traverseNode(data, selectedNode);
 
 console.log(data, 'current node is 2');
 
-traversingNode(data, '1-1');
+traverseNode(data, '1-1');
 
 console.log(data, 'current node is 1-1');
